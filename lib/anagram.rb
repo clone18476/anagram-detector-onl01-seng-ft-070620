a6995e28ec8c1f245d7d35e67c4aff0569d9ec09
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-08 20:47:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,10 @@
 # Your code goes here!
-class Anagram
+class Anagram     
+    # creates an Anagram class
+  attr_accessor :word     # sets an accessor method, word
 
-  attr_accessor :word
-
-  def initialize(word)
-    @word = word
+  def initialize(word)    # initialize to MUST start up with an argument
+    @word = word    # sets the argument to an instance variable, @word
   end
 
   def match(word_array)
